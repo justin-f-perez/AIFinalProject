6d297171c247d48ce82a13cea2d4735748dfed2c
miaomzou <89532887+miaomzou@users.noreply.github.com>
2022-05-05 20:00:26 -0500

    dfs

--- agents.py
+++ agents.py
@@ -1,12 +1,14 @@
 import abc
 import copy
 import logging
+import math
 from statistics import mean
 from typing import Callable, Iterable, NamedTuple
 
+from Algorithm import Algorithm
 from game import Game
 from snake import Snake
-from utils import Direction, PriorityQueue, manhattan_distance, reciprocal
+from utils import Direction, Grid, Node, PriorityQueue, manhattan_distance, reciprocal
 
 
 class BaseAgent(abc.ABC):
@@ -216,3 +218,52 @@ class PathNode(NamedTuple):
     actions: tuple[Direction, ...]
     state: Game
     cost: float
+
+
+class DFS(Algorithm):
+    def __init__(self, grid):
+        super().__init__(self, grid)
+
+    def recursive_DFS(self, snake: Snake, goalstate, currentstate):
+        # check if goal state
+        if currentstate == goalstate:
+            return self.get_path(currentstate)
+
+        # if already visted return
+        if currentstate in self.explored_set:
+            return None
+
+        self.explored_set.append(currentstate)  # mark visited
+        neighbors = self.get_neighbors(currentstate)  # get neighbors
+
+        # for each neighbor
+        for neighbor in neighbors:
+            if not self.inside_body(snake, neighbor) and neighbor not in self.explored_set:
+                neighbor.parent = currentstate  # mark parent node
+                path = self.recursive_DFS(snake, goalstate, neighbor)  # check neighbor
+                if path != None:
+                    return path  # found path
+        return None
+
+    def run_algorithm(self, snake):
+        # to avoid looping in the same location
+        if len(self.path) != 0:
+            # while you have path keep going
+            path = self.path.pop()
+
+            if self.inside_body(snake, path):
+                self.path = []  # or calculate new path!
+            else:
+                return path
+
+        # start clean
+        self.frontier = []
+        self.explored_set = []
+        self.path = []
+
+        initialstate, goalstate = self.get_initstate_and_goalstate(snake)
+
+        self.frontier.append(initialstate)
+
+        # return path
+        return self.recursive_DFS(snake, goalstate, initialstate)
